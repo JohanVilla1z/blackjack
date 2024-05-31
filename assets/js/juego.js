1b6a0563a333d18e52a3dc4c61c4352f159a5329
JohanVilla1z <johanvilla64@gmail.com>
2024-05-31 16:32:01 -0500

    fully functional BlackJack game

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -13,10 +13,17 @@ let puntosJugador = 0,
     puntosComputadora = 0;
 
 //Rerferencias HTML
-const btnPedir = document.querySelector('#btnPedir');  //boton pedir carta
-const divCartasJugador = document.querySelector('#jugador-cartas'); //cartas jugador
-const puntos = document.querySelectorAll('small'); //puntos de jugador y computadora
+const btnPedir              = document.querySelector('#btnPedir');              //boton pedir carta
+const btnDetener            = document.querySelector('#btnDetener');            //boton detener
+const btnNuevoJuego         = document.querySelector('#btnNuevo');              //Nuevo juego
+const divCartasJugador      = document.querySelector('#jugador-cartas');        //cartas jugador
+const divCartasComputadora  = document.querySelector('#computadora-cartas');    //cartas jugador
+const puntos                = document.querySelectorAll('small');               //puntos de jugador y computadora
 
+const CartaInicial = document.createElement('img');
+CartaInicial.src = `assets/cartas/red_back.png`;
+CartaInicial.classList.add('carta-inicial');
+divCartasJugador.append(CartaInicial);
 
 const crearDeck = () => {
 
@@ -41,12 +48,8 @@ crearDeck();
 //esta funcion me permite tomar una carta
 
 const pedirCarta = () => {
-
-    (deck.length === 0) ? console.log('No hay cartas en el deck') : console.log('');
-
     const carta = deck.pop();
     return (carta);
-
 };
 
 const valorCarta = (carta) => {
@@ -56,29 +59,80 @@ const valorCarta = (carta) => {
 
 };
 
-const carta = pedirCarta();
-console.log({ carta }, valorCarta(carta));
 
+//Turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+    do {
+            const carta = pedirCarta();
 
+            puntosComputadora += valorCarta(carta);
+            puntos[1].innerText = puntosComputadora;
 
+            const imgCarta = document.createElement('img');
+            imgCarta.src = `assets/cartas/${carta}.png`;
+            imgCarta.classList.add('carta');
+            divCartasComputadora.append(imgCarta);
+    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+
+    //evaluacion del ganador
+    setTimeout(() => {
+        
+        if (puntosComputadora === puntosJugador) {
+            alert('Nadie gana');
+        } else if (puntosJugador > 21) {
+            alert('Computadora gana');
+        } else if (puntosComputadora > 21) {
+            alert('Jugador gana');
+        } else {alert('Computadora gana');}
+    }, 200);
+    
+};
 //Eventos
 btnPedir.addEventListener('click', () => {
-
     const carta = pedirCarta();
-
-    console.log({ carta }, valorCarta(carta));
-
+    console.log({ carta }, valorCarta(carta));                      //TODO Borrar
     puntosJugador += valorCarta(carta);
-
     puntos[0].innerText = puntosJugador;
-
+    
     const imgCarta = document.createElement('img');
     imgCarta.src = `assets/cartas/${carta}.png`;
     imgCarta.classList.add('carta');
-
     divCartasJugador.append(imgCarta);
+    CartaInicial.remove();
 
-    (puntosJugador > 21) ? btnPedir.disabled = true : null;
+
+    if (puntosJugador > 21 ){
+        console.warn('Perdiste');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
+    } else if (puntosJugador === 21) {
+        console.warn('21, genial');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
+    }
+
+});
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+}); 
+
+btnNuevoJuego.addEventListener('click', () => {
+    deck = [];
+    deck = crearDeck();
+    puntosJugador = 0;
+    puntosComputadora = 0;
+    puntos[0].innerText = 0;
+    puntos[1].innerText = 0;
+    divCartasComputadora.innerHTML = '';
+    divCartasJugador.innerHTML = '';
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+    divCartasJugador.append(CartaInicial);
 
 });
 
